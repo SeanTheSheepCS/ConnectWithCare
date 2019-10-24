87c368c11ee8a08b3918058cc0de614601b47c45
Daryl <43867408+dellod@users.noreply.github.com>
2019-10-24 15:27:01 -0600

    Added to ClientController.
    Base connection functionality to Server, cannot communicate yet.

---
+++ ClientFiles/ClientController.cpp
@@ -0,0 +1,77 @@
+/*
+ * ConnectWithCare
+ * "ClientController.cpp"
+ * 
+ * TCP type client.
+ * 
+ * Last updated by: Daryl
+ */
+#include "ClientController.h"
+#include <iostream>
+
+using namespace std;
+
+ClientController::ClientController(int portNumber, char* serverIP)
+{
+    createSocket();
+
+    /* Initialize the server information. Server ports less than 1023 are not allowable unless granted privileges. */ 
+    memset(&serverAddr, 0, sizeof(serverAddr)); // Zero out the structure
+    serverAddr.sin_family = AF_INET; // Use Internet address family
+    serverAddr.sin_port = htons(portNumber); // Server port number
+    serverAddr.sin_addr.s_addr = inet_addr(serverIP); // Server IP address
+
+    /* Connect to the server
+     * sock: the socket for this connection.
+     * serverAddr: the server address.
+     * sizeof(*): the size fo the rserver address
+     */
+    if (connect(sock, (struct sockaddr *) &serverAddr, sizeof(serverAddr)) < 0)
+    {
+        cout << "connect() failed" << endl;
+        exit(1);
+    }
+    cout<< "ENTER TEST MESSAGE TO SERVER ('exit' to quit): ";
+}
+
+void communicate()
+{
+    
+}
+
+void ClientController::createSocket()
+{
+    /* Create a TCP socket
+     * AF_INET: using address family "Internet Protocol address"
+     * SOCK_STREAM: Provides sequenced, reliable, bidirectional, connection-mode byte streams.
+     * IPPROTO_TCP: TCP protocol
+     */
+    if ((sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+    {
+        cout << "socket() failed" << endl;
+        exit(1);
+    }
+
+    /* Free up the port before binding
+     * sock: the socket just created
+     * SOL_SOCKET: set the protocol level at the socket level
+     * SO_REUSEADDR: allow reuse of local addresses
+     * &yes: set SO_REUSEADDR on a socket to true (1)
+     * sizeof(int): size of the value pointed by "yes"
+     */
+    int yes = 1;
+    if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) < 0)
+    {
+        cout << "setsockopt() failed" << endl;
+        exit(1);
+    }
+}
+int main(int argc, char *argv[])
+{
+    if(argc != 3)
+    {
+        cout << "Invalid input (<Server IP> <Server Port>)." << endl;
+        exit(1); // Exits program, failure.
+    }
+    ClientController theClient(atoi(argv[2]), argv[1]); // Just an example right now.
+}
---
+++ ClientFiles/ClientController.h
@@ -0,0 +1,47 @@
+/*
+ * ConnectWithCare
+ * "ClientController.h"
+ * 
+ * TCP type client.
+ * 
+ * Last updated by: Daryl
+ */
+
+
+#ifndef CLIENT_CONTROLLER
+#define CLIENT_CONTROLLER
+
+//#include "ClientMessageConverter.h" // Using converted to send messages over.
+#include <sys/socket.h> // for socket(), connect(), send(), and recv()
+#include <arpa/inet.h>  // for sockaddr_in and inet_addr()
+#include <stdlib.h>     // for atoi() and exit()
+#include <string.h>     // for memset()
+#include <unistd.h>     // for close()
+
+#define BUFFERSIZE 32
+
+
+class ClientController
+{
+    public:
+        ClientController(int portNumber, char* serverIP);
+        // Constructs ClientController.
+
+        void communicate();
+        // Communicates functionalities with clients.
+        
+    private:
+        int sock; // Socket descriptor
+        struct sockaddr_in serverAddr; // Address of the server
+        char inBuffer[BUFFERSIZE]; // Buffer for the message from the server
+        int bytesRecv; // Number of bytes received
+
+        char outBuffer[BUFFERSIZE]; // Buffer for message to the server
+        int msgLength; // Length of the outgoing message
+        int bytesSent; // Number of bytes sent
+
+        void createSocket();
+        // Helper function for creating the socket.
+};
+
+#endif
