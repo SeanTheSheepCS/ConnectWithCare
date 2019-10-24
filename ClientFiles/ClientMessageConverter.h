96a53f35a19e13d06d985fede5a427e4fb8239b8
SeanTheSheepCS <seankenny6688@gmail.com>
2019-10-24 16:11:32 -0600

    Structure overhaul in progress...

--- ClientFiles/ClientMessageConverter.h
+++ ClientFiles/ClientMessageConverter.h
@@ -7,10 +7,21 @@ class ClientMessageConverter
 {
   public:
     ClientMessageConverter();
+    bool isLoginAuthMessage(Message messageToDetermineTypeOf);
+    bool isLogoutConfirmMessage(Message messageToDetermineTypeOf);
+    bool isMessageDataMessage(Message messageToDetermineTypeOf);
+    bool isMessageImageDataMessage(Message messageToDetermineTypeOf);
+    bool isPostingDataMessage(Message messageToDetermineTypeOf);
+    bool isPostingImageDataMessage(Message messageToDetermineTypeOf);
+    bool isErrorNoAuthMessage(Message messageToDetermineTypeOf);
+    bool isErrorUserNotFoundMessage(Message messageToDetermineTypeOf);
+    bool isErrorBoardNotFoundMessage(Message messageToDetermineTypeOf);
+    bool isErrorWriteFailedMessage(Message messageToDetermineTypeOf);
+    bool isWriteSuccessfulMessage(Message messageToDetermineTypeOf);
+    bool isEndOfDataMessage(Message messageToDetermineTypeOf);
   protected:
     //
   private:
-    //
 }
 
 #endif
--- ServerFiles/ServerMessageConverter.h
+++ ServerFiles/ServerMessageConverter.h
@@ -9,35 +9,15 @@ class ServerMessageConverter
   public:
     ServerMessageConverter();
     bool isALoginMessage(Message messageToDetermineTypeOf);
-    std::string getUsernameOfALoginMessage(Message loginMessageToGetUsernameFieldOf);
-    std::string getPasswordOfALoginMessage(Message loginMessageToGetPasswordFieldOf);
-    std::vector<std::string> getUsernameInFirstElementOfVectorAndPasswordInSecondElementOfVectorWhenYouPassInALoginMessage(Message loginMessageToGetUsernameAndPasswordFieldOf);
-
     bool isALogoutMessage(Message messageToDetermineTypeOf);
-
     bool isACreatePostingMessage(Message messageToDetermineTypeOf);
-    Posting turnCreatePostingMessageIntoPosting(Message postingMessageToTurnIntoPosting);
-
     bool isACreatePostingImageMessage(Message messageToDetermineTypeOf);
-    PostingImage turnCreatePostingImageMessageIntoPostingImage(Message postingImageMessageToTurnIntoPostingImage);
-
     bool isASendUserMessageMessage(Message messageToDetermineTypeOf);
-    UserMessage turnSendUserMessageMessageIntoUserMessage(Message sendUserMessageToConvertToUserMessage);
-
     bool isASendUserMessageImageMessage(Message messageToDetermineTypeOf);
-    UserMessageImage turnSendUserMessageImageMessageIntoUserMessageImage(Message sendUserMessageImageMessageToTurnIntoUserMessageImage);
-
     bool isAMessageHistoryMessage(Message messageToDetermineTypeOf);
-    std::string getUsernamePortionOfTheMessageHistoryMessage(Message userHistoryMessageWhoseUsernameFieldWeWantToGet);
-
     bool isAMessageHistoryAllMessage(Message messageToDetermineTypeOf);
-
     bool isABoardHistoryAllMessage(Message messageToDetermineTypeOf);
-    long int getTheBoardIDFieldOfABoardHistoryAllMessage(Message boardHistoryAllMessageToGetIDFieldFrom);
-
     bool isABoardSearchMessage(Message messageToDetermineTypeOf);
-    std::string getKeywordOfBoardSearchMessage(Message boardSearchMessageToGetKeywordFieldOf);
-    long int getTheBoardIDFieldOfABoardSearchMessage(Message boardSearchMessageToGetBoardIDFieldOf);
   protected:
     //
   private:
