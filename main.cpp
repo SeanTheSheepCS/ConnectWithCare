ce787fa72e6ac18fe8bab197e9602722865903eb
SeanTheSheepCS <seankenny6688@gmail.com>
2019-11-08 12:35:36 -0700

    changed main.cpp to client mode (maybe fixes merge conflict?)

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "ServerFiles/ServerController.h"
 #include "CommonFiles/UnitTests/MessageCreatorsAndConvertersTest.h"
 
-#define UNIT_TEST_MODE
+#define CLIENT_MODE
 
 int main(int argc, char** argv)
 {
